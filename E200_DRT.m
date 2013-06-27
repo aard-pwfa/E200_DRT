0c95ee90bd7cf67c52796b9b0904b13ea571498a
Joel Frederico <joelfred@slac.stanford.edu>
2013-06-27 12:48:56 -0700

    Updated fig file names the *right* way./Updated .gitignore.

--- E200_DRT.m
+++ E200_DRT.m
@@ -1,35 +1,35 @@
-function varargout = E200_Analysis(varargin)
-% E200_ANALYSIS M-file for E200_Analysis.fig
-%      E200_ANALYSIS, by itself, creates a new E200_ANALYSIS or raises the existing
+function varargout = E200_DRT(varargin)
+% E200_DRT M-file for E200_DRT.fig
+%      E200_DRT, by itself, creates a new E200_DRT or raises the existing
 %      singleton*.
 %
-%      H = E200_ANALYSIS returns the handle to a new E200_ANALYSIS or the handle to
+%      H = E200_DRT returns the handle to a new E200_DRT or the handle to
 %      the existing singleton*.
 %
-%      E200_ANALYSIS('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in E200_ANALYSIS.M with the given input arguments.
+%      E200_DRT('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in E200_DRT.M with the given input arguments.
 %
-%      E200_ANALYSIS('Property','Value',...) creates a new E200_ANALYSIS or raises
+%      E200_DRT('Property','Value',...) creates a new E200_DRT or raises
 %      the existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before E200_Analysis_OpeningFcn gets called.  An
+%      applied to the GUI before E200_DRT_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to E200_Analysis_OpeningFcn via varargin.
+%      stop.  All inputs are passed to E200_DRT_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help E200_Analysis
+% Edit the above text to modify the response to help E200_DRT
 
-% Last Modified by GUIDE v2.5 12-May-2013 23:16:59
+% Last Modified by GUIDE v2.5 27-Jun-2013 12:47:52
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @E200_Analysis_OpeningFcn, ...
-                   'gui_OutputFcn',  @E200_Analysis_OutputFcn, ...
+                   'gui_OpeningFcn', @E200_DRT_OpeningFcn, ...
+                   'gui_OutputFcn',  @E200_DRT_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -43,15 +43,15 @@ else
 end
 % End initialization code - DO NOT EDIT
 
-% --- Executes just before E200_Analysis is made visible.
-function E200_Analysis_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before E200_DRT is made visible.
+function E200_DRT_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to E200_Analysis (see VARARGIN)
+% varargin   command line arguments to E200_DRT (see VARARGIN)
 
-% Choose default command line output for E200_Analysis
+% Choose default command line output for E200_DRT
 handles.output = hObject;
 
 % Update handles structure
@@ -59,12 +59,12 @@ guidata(hObject, handles);
 
 initialize_gui(hObject, handles, false);
 
-% UIWAIT makes E200_Analysis wait for user response (see UIRESUME)
+% UIWAIT makes E200_DRT wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = E200_Analysis_OutputFcn(hObject, eventdata, handles)
+function varargout = E200_DRT_OutputFcn(hObject, eventdata, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
