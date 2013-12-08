512df04482acb4eb646066c3e2b8f0ddbbc70fac
Joel Frederico <joelfred@slac.stanford.edu>
2013-12-07 20:59:01 -0800

    Energy axis works, only for recent data.

--- plotpanel.m
+++ plotpanel.m
@@ -47,13 +47,29 @@ function handles=plotpanel(hObject,handles)
 		% img;
 		% Determine spectrometer bend settings/calibration
 		bend_struct = data.raw.scalars.LI20_LGPS_3330_BDES;
-		bool        = ismember(wanted_UIDs,bend_struct.UID);
+		% bool        = ismember(wanted_UIDs,bend_struct.UID);
 		% display(bend_struct.UID)
 		B5D36 = bend_struct.dat{1};
-		% display(B5D36);
-		e_axis=E200_cher_get_E_axis('20130423','CELOSS',0,[1:1392],0,B5D36);
 		
-		[Erange, Eres, Dy] = E200_cher_get_E_axis(datename, camname, visu,  pixelrange, offset, sbend_setting)
+		% Get imgstruct for current options.
+		camind    = get(handles.Cams,'Value');
+		datatype  = handles.CamsLookup.datatype{camind};
+		name      = handles.CamsLookup.name{camind};
+
+		e_axis=E200_cher_get_E_axis('20131125',name,0,[1:1392],0,B5D36)
+
+		[ysize,xsize] = size(img);
+		
+		xticks = 0:xsize/5:xsize;
+		xticklabels = e_axis(xticks+1)
+		xticklabelstr = {};
+		for i=1:size(xticklabels,2)
+			xticklabelstr = [xticklabelstr sprintf('%03.2f',xticklabels(i))];
+		end
+		set(handles.fig1,'XTick',xticks);
+		set(handles.fig1,'XTickLabel',xticklabelstr);
+
+		% [Erange, Eres, Dy] = E200_cher_get_E_axis(datename, camname, visu,  pixelrange, offset, sbend_setting)
 	end
 	
 	contents = cellstr(get(handles.yunits,'String'));
