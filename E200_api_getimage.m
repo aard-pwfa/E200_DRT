2f2c2a936ad9ed5c1e2b3ea37669f84e0db350d5
Joel Frederico <joelfred@slac.stanford.edu>
2013-12-20 18:27:11 -0800

    Updated with varargin E200_api_getimage

--- E200_api_getimage.m
+++ E200_api_getimage.m
@@ -1,6 +1,10 @@
-function img = E200_api_getimage()
-	h = msgbox('Click global handles button.');
-	global handles
+function img = E200_api_getimage(varargin)
+	if nargin==1
+		handles=varargin{1};
+	else
+		h = msgbox('Click global handles button.');
+		global handles
+	end
 	imgnum=int32(get(handles.imageslider,'Value'));
 	img=handles.images{imgnum};
 	img=rot90(img);
